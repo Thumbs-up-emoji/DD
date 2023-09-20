1e0ad0f290b17910d5e48baab338e99987ff39f7
Vineet Krishna <f20221197@goa.bits-pilani.ac.in>
2023-09-20 08:51:18 +0530

    tut_3

---
+++ VT3/mux/mux_2to1.v
@@ -0,0 +1,8 @@
+module mux_2to1(input [1:0]in,input select,output out);
+
+    wire a1,a2;
+    wire n_s;
+
+    
+    
+endmodule
---
+++ VT3/mux/mux_4to1.v
@@ -0,0 +1,6 @@
+`include "mux_2to1.v"
+module mux_4to1(input [3:0] in,input [1:0] select,output out);
+    wire [1:0] m;
+
+    
+endmodule
