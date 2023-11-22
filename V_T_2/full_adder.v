aa659fde86ee3d81b597062bf11d0ece496d8cea
Vineet Krishna <f20221197@goa.bits-pilani.ac.in>
2023-11-22 22:36:25 +0530

    _

--- V_T_2/full_adder.v
+++ V_T_2/full_adder.v
@@ -19,7 +19,7 @@ module full_adder(input A,input B,input C0,output C1,output S);
     
     wire CMID;
     wire W1, W2;
-    half adder h1(A,B,CMID,W1);
-    half adder h2(C0,W1,W2,S);
+    half_adder h1(A,B,CMID,W1);
+    half_adder h2(C0,W1,W2,S);
     or o1(C1,CMID,W2);
 endmodule
