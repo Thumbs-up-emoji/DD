73d857f9e55e213087200e0670edccab3646a073
Vineet Krishna <f20221197@goa.bits-pilani.ac.in>
2023-11-23 19:06:17 +0530

    added 1 starting

--- V_T_2/4bit_adder.v
+++ V_T_2/4bit_adder.v
@@ -1,5 +1,5 @@
 `include "full_adder.v"
-module bit4_adder(input [3:0]A,input  [3:0]B,input C0,output  [3:0]O, output C1);
+module bit4_adder(input [3:0]A,input  [3:0]B,input C0,output  [3:0]S, output C1);
     wire C1,C2,C3;
     full_adder fa1 (A[0],B[0],Cin,S[0],C1);
     full_adder fa2 (A[1],B[1],C1,S[1],C2);
